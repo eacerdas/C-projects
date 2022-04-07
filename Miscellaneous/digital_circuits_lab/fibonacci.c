366efbc2138ce79a9bcd2acebc07751cd3e09bd5
Edward Cerdas <65683552+eacerdas@users.noreply.github.com>
2022-04-07 11:24:57 -0600

    Changes the code to another because of some issues

--- Miscellaneous/digital_circuits_lab/fibonacci.c
+++ Miscellaneous/digital_circuits_lab/fibonacci.c
@@ -1,24 +1,34 @@
 #include <stdint.h>
-#include <stdio.h>
 
-int main (){
-    /* Declaracion de variables */
-    uint32_t fibonacci = 0; 
-    uint32_t firstNumber = 0;
-    uint32_t secondNumber = 1;
-    uint32_t max_number = 10946;
-    volatile uint32_t *memoryAddress = ( uint32_t *) 0x10000000; // Direccion de guardado
+#define LED_REGISTERS_MEMORY_ADD 0x10000000
+#define LOOP_WAIT_LIMIT 100
 
-    *memoryAddress = fibonacci; // Cero es el primer numero en ser agregado
-    while (fibonacci < max_number){
-        fibonacci = firstNumber + secondNumber; // Obtiene numero fibonacci
-        firstNumber = secondNumber; // Siguiente iteracion
-        secondNumber = fibonacci;
+static void putuint(uint32_t i)
+{
+    *((volatile uint32_t *)LED_REGISTERS_MEMORY_ADD) = i;
+}
 
-        /* Revisa si es par: si lo es, se agrega, sustituyendo lo que haya 
-        en la dirección 0x10000000 */
-        if (fibonacci % 2 == 0){
-            *memoryAddress = fibonacci;
-        } 
+void main()
+{
+    uint32_t counter = 0;
+    uint32_t t1 = 0, t2 = 1; //first two elements of the series
+    uint32_t nextTerm = t1 + t2; //3rd element of the series
+    while (1)
+    {
+        if(nextTerm%2 == 0)
+		    putuint(nextTerm);
+        if(nextTerm == 10946)
+        {
+            t1 = 0, t2 = 1;
+            nextTerm = t1 + t2;
+        }	
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
+        counter = 0;
+        while (counter < LOOP_WAIT_LIMIT)
+        {
+            counter++;
+        }
     }
 }
