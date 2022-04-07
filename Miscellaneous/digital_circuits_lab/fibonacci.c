a22173c4e93e533a836781ebf8959488e4bc00e4
Edward Cerdas <65683552+eacerdas@users.noreply.github.com>
2022-04-07 08:27:04 -0600

    Fixes some issues due to variable names

--- Miscellaneous/digital_circuits_lab/fibonacci.c
+++ Miscellaneous/digital_circuits_lab/fibonacci.c
@@ -1,8 +1,5 @@
-#include <stdint .h>
-#include <stdio .h>
-
-/* Directivas de pre - procesamiento */
-# define uint32_t // 32 - bit word core
+#include <stdint.h>
+#include <stdio.h>
 
 int main (){
     /* Declaracion de variables */
@@ -10,18 +7,18 @@ int main (){
     uint32_t firstNumber = 0;
     uint32_t secondNumber = 1;
     uint32_t max_number = 10946;
-    volatile uint32_t *v = ( uint32_t *) 0x10000000; // Direccion de guardado
+    volatile uint32_t *memoryAddress = ( uint32_t *) 0x10000000; // Direccion de guardado
 
-    *memoryAddress = f ; // Dero es el primer numero en ser agregado
-    while (f < max_number){
+    *memoryAddress = fibonacci; // Cero es el primer numero en ser agregado
+    while (fibonacci < max_number){
         fibonacci = firstNumber + secondNumber; // Obtiene numero fibonacci
         firstNumber = secondNumber; // Siguiente iteracion
         secondNumber = fibonacci;
-        
+
         /* Revisa si es par: si lo es, se agrega, sustituyendo lo que haya 
         en la dirección 0x10000000 */
-        if (f % 2 == 0){
-            *memoryAddress = f;
+        if (fibonacci % 2 == 0){
+            *memoryAddress = fibonacci;
         } 
     }
 }
