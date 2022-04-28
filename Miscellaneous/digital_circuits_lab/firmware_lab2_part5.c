c8286e98f8dbd7101604fc2e4e27a4f83761d14d
Edward Cerdas <65683552+eacerdas@users.noreply.github.com>
2022-04-28 11:01:55 -0600

    Update firmware_lab2_part5.c

--- Miscellaneous/digital_circuits_lab/firmware_lab2_part5.c
+++ Miscellaneous/digital_circuits_lab/firmware_lab2_part5.c
@@ -1,9 +1,9 @@
 #include <stdint.h>
 
-#define LED_REGISTERS_MEMORY_ADD 0 x10000000
-#define DIR_REG1 0 x0FFFFFF0
-#define DIR_REG2 0 x0FFFFFF4
-#define DIR_SAVE 0 x0FFFFFF8
+#define LED_REGISTERS_MEMORY_ADD 0x10000000
+#define DIR_REG1 0x0FFFFFF0
+#define DIR_REG2 0x0FFFFFF4
+#define DIR_SAVE 0x0FFFFFF8
 
 void main () {
 
@@ -15,17 +15,17 @@ void main () {
 
   /* Escribir los numeros del 0 al 15 */
 
-   for ( counter1 = 0 ; counter1 <16; counter1 ++){
-   pointer1 = (uint32_t *) DIR_REG1;
+  for ( counter1 = 0 ; counter1 <16; counter1 ++){
+  pointer1 = (uint32_t *) DIR_REG1;
   * pointer1 = counter1;
 
-     for ( counter2 = 0 ; counter2 <16; counter2 ++) {
-     pointer2 = (uint32_t *) DIR_REG2;
-     * pointer2 = counter2;
+    for ( counter2 = 0 ; counter2 <16; counter2 ++) {
+    pointer2 = (uint32_t *) DIR_REG2;
+    * pointer2 = counter2;
 
-    pointerMEM_ADD = (uint32_t *) LED_REGISTERS_MEMORY_ADD ;
-    * pointerMEM_ADD = *((volatile uint32_t *) DIR_SAVE ) ;
-
+    pointerMEM_ADD = (uint32_t *) LED_REGISTERS_MEMORY_ADD;
+    * pointerMEM_ADD = *((volatile uint32_t *) DIR_SAVE);
+    
     }
   }
 }
