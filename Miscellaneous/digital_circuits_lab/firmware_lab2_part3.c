376411918f6a19b986c48dc519851d2135ec693b
Edward Cerdas <65683552+eacerdas@users.noreply.github.com>
2022-04-28 07:55:45 -0600

    Update firmware_lab2_part3.c

--- Miscellaneous/digital_circuits_lab/firmware_lab2_part3.c
+++ Miscellaneous/digital_circuits_lab/firmware_lab2_part3.c
@@ -11,19 +11,14 @@ int main ( void )
 volatile word32 * wr_addr4 = ( word32 *) LED_REGISTERS_MEMORY_ADD4 ; // direccion donde se escribe el resultado
 volatile word32 * wr_addr8 = ( word32 *) LED_REGISTERS_MEMORY_ADD8 ; //direccion donde se escribe el resultado
 
-/*
-realiza las multiplicaciones
-de los numeros solicitados
-*/
+/* realiza las multiplicaciones
+de los numeros solicitados */
 * wr_addr4 = 1;
 * wr_addr8 = 7;
 * wr_addr4 = 2;
 * wr_addr8 = 15;
 * wr_addr4 = 6;
 * wr_addr8 = 10;
-
-* wr_addr8 = 10;
   
 return 0;
-
 }
