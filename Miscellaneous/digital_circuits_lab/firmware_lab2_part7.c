bc555a4eff4be9447319c642709afa7c1958a828
Edward Cerdas <65683552+eacerdas@users.noreply.github.com>
2022-05-05 09:34:44 -0600

    Update firmware_lab2_part7.c

--- Miscellaneous/digital_circuits_lab/firmware_lab2_part7.c
+++ Miscellaneous/digital_circuits_lab/firmware_lab2_part7.c
@@ -1,75 +1,44 @@
-"include "lut_multiplier_32b.v"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-module factorial (
-    input clk,
-    input reset,
-    input [31:0] source,
-    input control,
-    output reg status,
-    output reg [63:0] out_fact64
-);
+#define A 0x0FFFFFE0         //NUMERO AL CUAL SE LE QUIRE CALCULAR EL FACTORIAL       
+#define RESULT 0x0FFFFFE8    //RESULTADO
+#define CONTROL 0x0FFFFFE4   //CONTROL                    
+#define STATUS 0x0FFFFFEC    //STATUS
+#define RESULT_FINAL 0x10000000   //RESULTADO_FINAL
 
-reg estado_presente;
-reg proximo_estado;
-reg [31:0] counter;
-reg [31:0] A_i;
-wire [63:0] out_fact_aux;
+/*Guarda el resultado en memoria*/
+static void putuint(uint32_t i, int DIR) {
+	*((volatile uint32_t *)DIR) = i;
+}
 
-parameter RESET = 0;    //0000
-parameter CALC = 1; //0001
+/*Carga el resultado de memoria*/
+static uint32_t getuint(int DIR) {
+	return *((volatile uint32_t *)DIR);
+}
 
-lut_multiplier_32b  a(
-	.reset (reset),
-	.A (A_i),
-	.B (counter),
-	.out_64b (out_fact_aux)	
-);
+static void factorial(uint32_t n){
+    putuint(0, STATUS);                              // Señal de status en 0
+    if(n == 0){
+        putuint(1, A);
+    }
+    else{
+        putuint(n, A);                          // almacena el numero en 0x0FFFFFE0 
+        putuint(1, CONTROL);                          // Cambia Control
+        while(1){  
+            if(getuint(STATUS) == 1) {               // Termina factorial
+                putuint(0, CONTROL);                  // Cambia Control
+                putuint(getuint(RESULT), RESULT_FINAL); //Guarda el resultado en 0X10000000
+                return;
+            }
+        }    
+    }
+}
 
-always @(posedge clk)begin
-    if (~reset)begin
-        estado_presente <= RESET;
-        out_fact64 <= 0;
-    end
-    else begin
-        estado_presente <= proximo_estado;
-        counter <= counter + 1;
-        A_i <= out_fact_aux;
-    end
-end
-
-always @(*)begin
-    proximo_estado = estado_presente;
-
-    case (estado_presente)
-        
-        RESET:begin
-            status = 0;
-            counter = 1;
-            A_i = 1;
-            if (~control)begin
-                proximo_estado = RESET;
-            end
-            else begin
-                proximo_estado = CALC;
-            end
-        end
-
-        CALC:begin
-            if (counter <= source)begin
-                proximo_estado = CALC;
-            end
-            else begin
-                proximo_estado = RESET;
-                status = 1;
-                out_fact64 = A_i;
-            end
-        end
-
-        default:begin
-            proximo_estado = RESET;
-        end
-
-    endcase
-end
-    
-endmodule
+void main(){
+    factorial(5);
+    factorial(7);
+    factorial(10);
+    factorial(12);
+}
